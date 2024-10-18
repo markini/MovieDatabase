9d69df0eccbce9d78926bfd373c11b5edea059f5
Daniel Markart <daniel.markart@bergfex.at>
2024-10-18 15:11:10 +0200

    refactor(moviedb): Make the favorite icon a proper iconbutton
    - For a better ripple effect

--- core/designsystems/src/main/java/at/marki/moviedb/core/designsystems/components/FavoriteIcon.kt
+++ core/designsystems/src/main/java/at/marki/moviedb/core/designsystems/components/FavoriteIcon.kt
@@ -1,15 +1,16 @@
 package at.marki.moviedb.core.designsystems.components
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Bookmark
 import androidx.compose.material.icons.filled.BookmarkBorder
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import at.marki.moviedb.core.designsystems.ThemePreviews
+import at.marki.moviedb.core.designsystems.theme.AppTheme
 import at.marki.moviedb.core.designsystems.theme.favoriteColor
 
 @Composable
@@ -18,12 +19,40 @@ fun FavoriteIcon(
     onToggleFavorite: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Icon(
-        imageVector = if (isFavorite) Icons.Default.Bookmark else Icons.Default.BookmarkBorder,
-        contentDescription = null,
-        tint = if (isFavorite) favoriteColor else LocalContentColor.current,
-        modifier = modifier
-            .clip(CircleShape)
-            .clickable { onToggleFavorite() },
-    )
+    IconButton(
+        onClick = onToggleFavorite,
+        modifier = modifier,
+    ) {
+        Icon(
+            imageVector = if (isFavorite) Icons.Default.Bookmark else Icons.Default.BookmarkBorder,
+            contentDescription = null,
+            tint = if (isFavorite) favoriteColor else LocalContentColor.current,
+        )
+    }
+}
+
+@ThemePreviews
+@Composable
+private fun FavoriteIconPreview() {
+    AppTheme {
+        Surface {
+            FavoriteIcon(
+                isFavorite = true,
+                onToggleFavorite = {},
+            )
+        }
+    }
+}
+
+@ThemePreviews
+@Composable
+private fun FavoriteIconNoFavoritePreview() {
+    AppTheme {
+        Surface {
+            FavoriteIcon(
+                isFavorite = false,
+                onToggleFavorite = {},
+            )
+        }
+    }
 }
